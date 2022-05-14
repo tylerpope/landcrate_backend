de146b0d715b3a0692471807a7b1d654b2e0d60e
Tyler Pope <popetylerjames@gmail.com>
2022-05-14 11:52:24 -0400

    Update user

--- api/routes/user/index.js
+++ api/routes/user/index.js
@@ -12,4 +12,44 @@ router.get(
   },
 );
 
+router.put(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    const userId = req.user.id;
+
+    const {
+      email,
+      password,
+    } = req.body;
+
+    try {
+      const user = await db.User.findOne({
+        where: {
+          id: userId,
+        },
+      });
+
+      if (!user) return res.status(500).send('An error has occured.');
+      const updates = {
+        password,
+      };
+
+      if (email) {
+        updates.email = email;
+      }
+
+      if (password) {
+        updates.password = password;
+      }
+
+      await user.update(updates);
+
+      return res.status(200).send('User successfully updated.');
+    } catch (err) {
+      return next(err);
+    }
+  },
+);
+
 module.exports = router;
